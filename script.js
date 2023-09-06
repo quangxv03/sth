88bb07a07e3460b0afb2716f3b7dd12de611c89b
quangxv03 <113871496+quangxv03@users.noreply.github.com>
2023-09-06 17:41:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("myButton");
+
+    button.addEventListener("click", function () {
+        alert("Bạn đã nhấp vào nút!");
+    });
+});
